21a304ac05fdaf14e80f2ed17301ad5b6a4ec0cb
Nikki1404 <81031507+Nikki1404@users.noreply.github.com>
2026-03-31 15:25:03 +0530

    Update updates.py

--- updates.py
+++ updates.py
@@ -3,3 +3,8 @@ us-central1-docker.pkg.dev/emr-dgt-autonomous-uctr1-snbx/asr-parakeet-v3/parakee
 
 docker push \
 us-central1-docker.pkg.dev/emr-dgt-autonomous-uctr1-snbx/asr-parakeet-v3/parakeet_asr_v3:0.0.1
+
+(base) root@EC03-E01-AICOE1:/home/CORP/re_nikitav/parakeet-asr-multilingual# docker push us-central1-docker.pkg.dev/emr-dgt-autonomous-uctr1-snbx/asr-parakeet-v3/parakeet_asr_realtime:0.0.1
+The push refers to repository [us-central1-docker.pkg.dev/emr-dgt-autonomous-uctr1-snbx/asr-parakeet-v3/parakeet_asr_realtime]
+Get "https://us-central1-docker.pkg.dev/v2/": read tcp 10.90.126.61:32810->74.125.68.82:443: read: connection reset by peer
+(base) root@EC03-E01-AICOE1:/home/CORP/re_nikitav/parakeet-asr-multilingual#
