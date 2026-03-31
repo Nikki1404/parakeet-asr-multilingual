81b82c86c7f6f2c98b3c9a5969aa1a8c82395495
Nikki1404 <81031507+Nikki1404@users.noreply.github.com>
2026-03-31 15:43:19 +0530

    Update updates.py

--- updates.py
+++ updates.py
@@ -8,6 +8,8 @@ us-central1-docker.pkg.dev/emr-dgt-autonomous-uctr1-snbx/asr-parakeet-v3/parakee
 The push refers to repository [us-central1-docker.pkg.dev/emr-dgt-autonomous-uctr1-snbx/asr-parakeet-v3/parakeet_asr_realtime]
 Get "https://us-central1-docker.pkg.dev/v2/": read tcp 10.90.126.61:32810->74.125.68.82:443: read: connection reset by peer
 (base) root@EC03-E01-AICOE1:/home/CORP/re_nikitav/parakeet-asr-multilingual#
+https://us-central1-docker.pkg.dev/v2/
+{"errors":[{"code":"UNAUTHORIZED","message":"not authenticated: No credential was supplied."}]}
 
 
 unset http_proxy
